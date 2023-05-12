f7c60deeac9f14fe52e7d2cdfe638bfbc05e461f
Amir reza <amirrezadm81@gmail.com>
2023-05-12 13:33:24 +0330

    add score

--- src/App.vue
+++ src/App.vue
@@ -61,38 +61,25 @@
                 </v-row>
               </v-col>
               <v-col cols="12" v-if="result">
+                <p>امتیاز : {{ result.score }}</p>
                 <p>مبلغ نهایی : {{ result.price.toLocaleString() }}</p>
                 <p>ضریب : {{ result.z }}</p>
               </v-col>
             </v-row>
           </v-col>
 
+          <!-- <v-col cols="12" lg="6">
+            <Score />
+          </v-col> -->
+
           <v-col cols="12" lg="6">
             <div class="d-flex justify-space-between">
               <h2>جدول ضرایب</h2>
               <div class="d-flex">
                 <v-btn small color="primary" @click="download" class="mx-1">دانلود ضرایب</v-btn>
-              <v-btn small color="success" @click="addNew = true">ایجاد</v-btn>
               </div>
 
-              <v-dialog v-model="addNew" width="500">
-                <v-card>
-                  <v-card-title>افزودن ضریب</v-card-title>
-                  <v-card-text>
-                    <v-row>
-                      <v-col cols="6">
-                        <v-text-field label="سال" v-model="year" />
-                      </v-col>
-                      <v-col cols="6">
-                        <v-text-field label="ضریب" v-model="z" />
-                      </v-col>
-                    </v-row>
-                    <v-btn color="success" @click="add" :disabled="!year || !z"
-                      >افزودن</v-btn
-                    >
-                  </v-card-text>
-                </v-card>
-              </v-dialog>
+              
             </div>
 
             <v-data-table
@@ -102,9 +89,6 @@
               no-data-text="هیچ ضریبی وجود ندارد"
               :items-per-page="16"
             >
-              <template #item.action="{item}">
-                <v-btn small class="ml-2" color="error" @click="remove(item.year)">حذف</v-btn>
-              </template>
             </v-data-table>
           </v-col>
         </v-row>
@@ -123,84 +107,94 @@
 
 <script>
 import { mapState } from "vuex";
+
 export default {
-  name: "App",
-  data: () => ({
-    header: [
-      { value: "year", text: "سال" },
-      { value: "z", text: "ضریب" },
-      { value: "action", text: "" },
-    ],
-    fromYear: "",
-    toYear: "",
-    price: "",
-    toYearZaraeb: [],
-    result: null,
-    fix: null,
-    addNew: false,
-    year: "",
-    z: "",
-  }),
-  computed: {
-    ...mapState(["zaraeb"]),
-  },
-  watch: {
-    fromYear() {
-      this.toYearZaraeb = this.zaraeb.filter((z) => z.year >= this.fromYear);
+    name: "App",
+    data: () => ({
+        header: [
+            { value: "year", text: "سال" },
+            { value: "z", text: "ضریب" },
+            { value: "action", text: "" },
+        ],
+        fromYear: "",
+        toYear: "",
+        price: "",
+        toYearZaraeb: [],
+        result: null,
+        fix: null,
+        addNew: false,
+        year: "",
+        z: "",
+    }),
+    computed: {
+        ...mapState(["zaraeb"]),
     },
-  },
-  methods: {
-    calculate() {
-      let z;
-      const bazeh = this.zaraeb
-        .map((z) => {
-          if (z.year >= this.fromYear && z.year <= this.toYear) {
-            return z;
-          }
-        })
-        .filter((z) => z != undefined);
-      if (bazeh.length > 1) {
-        z = (
-          bazeh.reduce((p, c) => {
-            if (typeof p == "number") {
-              return p + c.z;
-            } else {
-              return p.z + c.z;
+    watch: {
+        fromYear() {
+            this.toYearZaraeb = this.zaraeb.filter((z) => z.year >= this.fromYear);
+        },
+    },
+    methods: {
+        calculate() {
+            let z;
+            const bazeh = this.zaraeb
+                .map((z) => {
+                if (z.year >= this.fromYear && z.year <= this.toYear) {
+                    return z;
+                }
+            })
+                .filter((z) => z != undefined);
+            if (bazeh.length > 1) {
+                z = (bazeh.reduce((p, c) => {
+                    if (typeof p == "number") {
+                        return p + c.z;
+                    }
+                    else {
+                        return p.z + c.z;
+                    }
+                }) / bazeh.length).toFixed(this.fix || 3);
             }
-          }) / bazeh.length
-        ).toFixed(this.fix || 3);
-      } else {
-        z = bazeh[0].z;
-      }
-      this.result = {
-        z,
-        price: this.price * z,
-      };
+            else {
+                z = bazeh[0].z;
+            }
+            // calculate score of this
+            let score = 0
+            const currentYear = 1402
+            console.log(((this.fromYear + this.toYear) / 2))
+            const p = (currentYear - ((this.fromYear + this.toYear) / 2)).toFixed(3)
+            console.log(Math.pow(1.2, p))
+            score = (0.03 * Math.pow(1.2, p) * this.price)
+            console.log(score)
+            this.result = {
+                z,
+                score,
+                price: this.price * z,
+            };
+        },
+        clear() {
+            this.fromYear = "";
+            this.toYear = "";
+            this.result = null;
+            this.fix = null;
+            this.price = null;
+            this.toYearZaraeb = [];
+        },
+        add() {
+            this.$store.commit("add", { year: this.year, z: this.z });
+            this.year = "";
+            this.z = "";
+            this.addNew = false;
+        },
+        remove(year) {
+            this.$store.commit("remove", year);
+        },
+        download() {
+            fetch("https://amirdm81.github.io/atd/data.json").then((data) => data.json()).then(({ data }) => {
+                this.$store.commit("import", data);
+            }).catch(e => {
+                console.log(e);
+            });
+        },
     },
-    clear() {
-      this.fromYear = "";
-      this.toYear = "";
-      this.result = null;
-      this.fix = null;
-      this.price = null;
-      this.toYearZaraeb = [];
-    },
-    add() {
-      this.$store.commit("add", { year: this.year, z: this.z });
-      this.year = "";
-      this.z = "";
-      this.addNew = false;
-    },
-    remove(year) {
-      this.$store.commit("remove", year);
-    },
-    download() {
-      fetch("https://amirdm81.github.io/atd/data.json").then((data) => data.json()).then(({data}) => {
-        this.$store.commit("import", data);
-      }).catch(e=> {
-        console.log(e);
-      })
-    },
-  },
 };
 </script>
---
+++ src/components/Score.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-card>
+        <v-card-title>محاسبه امتیاز</v-card-title>
+
+        
+    </v-card>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
